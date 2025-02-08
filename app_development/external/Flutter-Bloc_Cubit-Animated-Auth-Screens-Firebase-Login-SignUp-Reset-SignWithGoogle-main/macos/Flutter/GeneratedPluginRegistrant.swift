8281bb8ef55f57fec143407f90d16ae5ab959426
flash-275 <vanshmalani27@gmail.com>
2025-02-08 18:22:05 +0530

    changes

--- app_development/external/Flutter-Bloc_Cubit-Animated-Auth-Screens-Firebase-Login-SignUp-Reset-SignWithGoogle-main/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_development/external/Flutter-Bloc_Cubit-Animated-Auth-Screens-Firebase-Login-SignUp-Reset-SignWithGoogle-main/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import google_sign_in_ios
 import network_info_plus
 import path_provider_foundation
 import rive_common
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
